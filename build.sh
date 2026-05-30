beed8904f7b8447c894c6f214e8fc5479475ad16
nurselidemir <nurselidemr@icloud.com>
2026-05-30 03:01:18 +0300

    Add optional demo data seeding on deploy

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [ "${SEED_DEMO_DATA:-false}" = "true" ]; then
+  python manage.py seed_demo_data
+fi
